cbb6b638f23211b54256b3831a7a0f5c9e925402
tienld235 <leductien1997123@gmail.com>
2019-01-10 17:01:41 +0700

    render User data to view index

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -5,9 +5,10 @@ class Controller_User extends Controller_Template
 
 	public function action_index()
 	{
-		$data["subnav"] = array('index'=> 'active' );
-		$this->template->title = 'User &raquo; Index';
+	    $users = Model_User::find('all');
+		$data["subnav"] = array('index'=> 'btn btn-warning');
+		$data["users"] = $users;
+		$this->template->title = 'UMS &raquo; User';
 		$this->template->content = View::forge('user/index', $data);
 	}
-
 }
--- fuel/app/views/user/index.php
+++ fuel/app/views/user/index.php
@@ -1,5 +1,30 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "index" ); ?>'><?php echo Html::anchor('user/index','Index');?></li>
-
-</ul>
-<p>Index</p>
+<?php echo Html::anchor('user/add', '<i class="fas fa-plus"></i>Add ', array('class' => Arr::get($subnav, "index"))); ?>
+<div class="mt-5">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($users as $user) { ?>
+            <tr>
+                <td><?= $user->id ?></td>
+                <td><?= $user->name ?></td>
+                <td><?= $user->phone ?></td>
+                <td><?= $user->email ?></td>
+                <td><?= $user->address ?></td>
+                <td>
+                    <a href="#" class="btn btn-success">Edit</a>
+                    <a href="#" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
